91c6feefbfc2577c767d1eaf04cbe2d3951e0b0b
dutra-diego <diiegodutra15@gmail.com>
2024-11-29 11:08:06 -0300

    add components

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,19 @@
-import { createBrowserRouter } from "react-router-dom"
-import { Dashboard } from "./pages/app/dashboard"
-import { SignIn } from "./pages/auth/sign-in"
-import { AppLayout } from "./pages/_layouts/app"
-import { AuthLayout } from "./pages/_layouts/auth"
-import { SignUp } from "./pages/auth/sign-up"
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
   {
     path: "/",
@@ -19,4 +23,4 @@ export const router = createBrowserRouter([
       { path: "/sign-up", element: <SignUp /> },
     ],
   },
-])
+]);
