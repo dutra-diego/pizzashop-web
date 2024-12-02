fff5bc1cd9e9dffd02346d42cd5bb3286f6d1112
dutra-diego <diiegodutra15@gmail.com>
2024-12-02 10:13:43 -0300

    add api

---
+++ pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
@@ -0,0 +1,107 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ResponsiveContainer, PieChart, Pie, BarChart, Cell } from "recharts";
+import colors from "tailwindcss/colors";
+const data = [
+  {
+    product: "Pepperoni",
+    amount: 40,
+  },
+  {
+    product: "Mussarela",
+    amount: 36,
+  },
+  {
+    product: "Marguerita",
+    amount: 10,
+  },
+  {
+    product: "Strogonoff",
+    amount: 25,
+  },
+
+  {
+    product: "Calabresa",
+    amount: 10,
+  },
+];
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+];
+export function PopularProductsChart() {
+  return (
+    <Card className="col-span-3">
+      <CardHeader className="flex-row items-center justify-between pb-0">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-base font-medium">
+            Produtos populares
+          </CardTitle>
+          <BarChart className="h-4 w-4 text-muted-foreground" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart data={data} style={{ fontsize: 12 }}>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180;
+                const radius = 12 + innerRadius + (outerRadius - innerRadius);
+                const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? "start" : "end"}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat("...")
+                      : data[index].product}{" "}
+                    ({value})
+                  </text>
+                );
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={` cell-${
+                      // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                      index
+                    }`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80"
+                  />
+                );
+              })}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ pizzashop-web/src/pages/app/orders/orders.tsx
@@ -0,0 +1,60 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
+
+export function Orders() {
+  const {data: result} = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  });
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]">/</TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado Há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return (
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    <OrderTableRow key={i} />
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+        </div>
+      </div>
+    </>
+  );
+}
